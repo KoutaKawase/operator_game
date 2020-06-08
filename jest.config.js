cd9ee81cb3c0aaa6fd7131c00ed36fd157fdaffa
KoutaKawase <suhrkawase@gmail.com>
2020-06-08 16:04:11 +0900

    ✨ tsconfigを整えた

--- jest.config.js
+++ jest.config.js
@@ -75,7 +75,9 @@ module.exports = {
 	// ],
 
 	// A map from regular expressions to module names that allow to stub out resources with a single module
-	// moduleNameMapper: {},
+  //moduleNameMapper: {
+  //  "^@App/(.*)$": '<rootDir>/src/$1'
+  //},
 
 	// An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
 	// modulePathIgnorePatterns: [],
@@ -108,7 +110,7 @@ module.exports = {
 	// restoreMocks: false,
 
 	// The root directory that Jest should scan for tests and modules within
-	rootDir: "./spec",
+	rootDir: "./",
 
 	// A list of paths to directories that Jest should use to search for files in
 	// roots: [
