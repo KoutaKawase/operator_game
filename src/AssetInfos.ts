271104910987cae9f65601aa36134c575e65d53f
KoutaKawase <suhrkawase@gmail.com>
2020-06-13 21:04:27 +0900

    ✨ スタートコール実装！

--- src/AssetInfos.ts
+++ src/AssetInfos.ts
@@ -1,3 +1,3 @@
 export const titleAssets = ['titleImage', 'tokimakure', 'tokimakure_glyphs', 'zzz', 'titleBgm'];
 export const descriptionAsset = ['descBack', 'description', 'description_glyphs'];
-export const gameAsset = [];
+export const gameAsset = ['countdown'];
--- src/GameSubScene.ts
+++ src/GameSubScene.ts
@@ -2,13 +2,30 @@ import { SubScene } from './SubScene';
 import type { SceneInfo } from './types/SceneInfo';
 
 export class GameSubScene extends SubScene {
+  private isInGame: boolean;
+  //カウントダウン音声のおおよその長さ この長さ分待ってからゲームを開始
+  static readonly COUNTDOWN_TIME = 4500;
+
   constructor(sceneInfo: SceneInfo) {
     super(sceneInfo);
+    this.isInGame = false;
   }
 
-  protected loadedHandler(): void {
-    //mock
-    console.log('MAIN GAME');
+  private runReadySound(): Promise<void> {
+    (this.assets['countdown'] as g.AudioAsset).play();
+
+    //play()だけだとすぐ次の処理にいってしまうのでサウンド分待って次にいくためsetTimeoutをしている
+    return new Promise((resolve) => {
+      this.setTimeout(() => {
+        this.isInGame = true;
+        resolve();
+      }, GameSubScene.COUNTDOWN_TIME);
+    });
+  }
+
+  protected async loadedHandler(): Promise<void> {
+    await this.runReadySound();
+    console.log(this.isInGame);
   }
 
   protected updateHandler(): void {
