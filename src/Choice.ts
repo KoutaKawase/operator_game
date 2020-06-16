3001f82ee452603cb74ddd73cb24ead537ccd75a
KoutaKawase <suhrkawase@gmail.com>
2020-06-16 23:20:27 +0900

    ✨ とりあえずできた

--- src/Choice.ts
+++ src/Choice.ts
@@ -1,35 +1,38 @@
 export class Choice {
   private scene: g.Scene;
-  private plus: g.Sprite;
-  private minus: g.Sprite;
-  private cross: g.Sprite;
-  private div: g.Sprite;
+  private readonly plus: g.Sprite;
+  private readonly minus: g.Sprite;
+  private readonly cross: g.Sprite;
+  private readonly div: g.Sprite;
+  static readonly marginRight = 24;
+  static readonly spriteWidth = 80;
+  static readonly y = 278;
 
   constructor(scene: g.Scene) {
     this.scene = scene;
     this.plus = new g.Sprite({
       scene,
       src: scene.assets['plus'],
-      x: 24,
-      y: 278,
+      x: Choice.marginRight,
+      y: Choice.y,
     });
     this.minus = new g.Sprite({
       scene,
       src: scene.assets['minus'],
-      x: 80 + 24,
-      y: 278,
+      x: Choice.spriteWidth + Choice.marginRight,
+      y: Choice.y,
     });
     this.cross = new g.Sprite({
       scene,
       src: scene.assets['cross'],
-      x: 80 * 2 + 24,
-      y: 278,
+      x: Choice.spriteWidth * 2 + Choice.marginRight,
+      y: Choice.y,
     });
     this.div = new g.Sprite({
       scene,
       src: scene.assets['div'],
-      x: 80 * 3 + 24,
-      y: 278,
+      x: Choice.spriteWidth * 3 + Choice.marginRight,
+      y: Choice.y,
     });
   }
 
