eeccf39473611d7e56a4082c88602428dbceb25d
KoutaKawase <suhrkawase@gmail.com>
2020-06-17 20:06:51 +0900

    🚧 totalTimeを95秒に設定！

--- src/utils/gameUtil.ts
+++ src/utils/gameUtil.ts
@@ -4,10 +4,11 @@ export type GameStateType = {
   score: number;
   isFinished: boolean;
   totalTimeLimit: number;
+  isAtsumaru: boolean;
 };
 
 export function initGameState(param: Param): void {
-  let totalTimeLimit = 150;
+  let totalTimeLimit = 95;
 
   if (param.sessionParameter.totalTimeLimit) {
     totalTimeLimit = param.sessionParameter.totalTimeLimit;
@@ -17,7 +18,9 @@ export function initGameState(param: Param): void {
     score: 0,
     isFinished: false,
     totalTimeLimit,
+    isAtsumaru: param.isAtsumaru,
   };
 
   g.game.vars.gameState = gameState;
+  console.log(g.game.vars.gameState.totalTimeLimit);
 }
