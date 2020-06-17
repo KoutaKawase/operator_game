71b059a14bbc8678d9c4e1eae6f9ce17163b9362
KoutaKawase <suhrkawase@gmail.com>
2020-06-17 19:47:36 +0900

    🚧 グローバルゲームステートにトータル制限時間を設定

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,5 @@ import { GameMainParameterObject } from './types/parameterObject';
 import { MainSceneController } from './MainSceneController';
 
 export function main(param: GameMainParameterObject): void {
-  param;
-  g.game.pushScene(MainSceneController.createMainScene(g.game));
+  g.game.pushScene(MainSceneController.createMainScene(g.game, param));
 }
--- src/utils/gameUtil.ts
+++ src/utils/gameUtil.ts
@@ -1,12 +1,23 @@
+import { GameMainParameterObject as Param } from '../types/parameterObject';
+
 export type GameStateType = {
   score: number;
   isFinished: boolean;
+  totalTimeLimit: number;
 };
 
-export function initGameState(): void {
+export function initGameState(param: Param): void {
+  let totalTimeLimit = 150;
+
+  if (param.sessionParameter.totalTimeLimit) {
+    totalTimeLimit = param.sessionParameter.totalTimeLimit;
+  }
+
   const gameState: GameStateType = {
     score: 0,
     isFinished: false,
+    totalTimeLimit,
   };
+
   g.game.vars.gameState = gameState;
 }
